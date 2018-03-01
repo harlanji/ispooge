10f2735696e2c38b854675301fff513b919eaa7e
Harlan Iverson <h.iverson@gmail.com>
2018-03-01 22:24:22 +0000

    added scripts about segmenting video

--- scripts/segment-video.sh
+++ scripts/segment-video.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# https://developer.apple.com/library/content/documentation/NetworkingInternet/Conceptual/StreamingMediaGuide/UsingHTTPLiveStreaming/UsingHTTPLiveStreaming.html
+
 # https://docs.peer5.com/guides/production-ready-hls-vod/
 
 # http://www.bogotobogo.com/VideoStreaming/ffmpeg_http_live_streaming_hls.php
@@ -7,20 +9,23 @@
 # https://www.encoding.com/blog/2014/02/24/simplify-http-live-streaming-encoding-com-hls-segmenter/
 
 
-# https://forums.roku.com/viewtopic.php?t=60698
+# detailed example of ffmpeg cli options: https://forums.roku.com/viewtopic.php?t=60698
+
+# details of GOP params: https://superuser.com/questions/908280/what-is-the-correct-way-to-fix-keyframes-in-ffmpeg-for-dash
 
 
-# https://blog.twitch.tv/live-video-transmuxing-transcoding-ffmpeg-vs-twitchtranscoder-part-i-489c1c125f28
+# example ffmpeg for multi-transcoding: https://blog.twitch.tv/live-video-transmuxing-transcoding-ffmpeg-vs-twitchtranscoder-part-i-489c1c125f28
 
 # http://www.streamingmedia.com/Articles/ReadArticle.aspx?ArticleID=95251&PageNum=3
 
-# Tinkerboard: https://tinkerboarding.co.uk/forum/archive/index.php/thread-51.html
+# Tinkerboard (decoders only): https://tinkerboarding.co.uk/forum/archive/index.php/thread-51.html
 
 
 # adding hls to ffmpeg: 
 # https://trac.ffmpeg.org/ticket/1642 
 # https://trac.ffmpeg.org/ticket/1642
 
+# https://trac.ffmpeg.org/wiki/EncodingForStreamingSites
 
 # rpi omx:  https://signal-flag-z.blogspot.com/2017/05/raspberry-pix264ffmpeg.html
 
