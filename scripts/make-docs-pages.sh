c12f86d8339d8645d713806daee37038b14a856a
Harlan Iverson <h.iverson@gmail.com>
2018-05-17 22:53:46 +0000

    doc pages not on nav bar

--- scripts/make-docs-pages.sh
+++ scripts/make-docs-pages.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 
-# we have run download-playlist.sh in $PLAYLIST_DIR
-# we have segmented each video in the playlist
-# there is a thumbnail for each downloaded video
-
 DOCS_DIR="${1:-doc}"
 
 
@@ -13,6 +9,7 @@ DOC_FILES=$( find $DOCS_DIR -maxdepth 1 -type f -name '*.md' )
 i=4000
 for docFile in $DOC_FILES; do
   export PAGE_INDEX="$i"
+  export IS_NAVBAR="false"
   baseName="$(basename -s .md $docFile)"
   pageFile="md-pages/doc-$baseName.md" # todo flatten
   
