6697cb63414572f8c7b0ccf53b971d0a547e8365
Harlan Iverson <h.iverson@gmail.com>
2018-07-28 16:11:45 -0700

    updated streaming config and volumes

--- scripts/web/start-streaming.sh
+++ scripts/web/start-streaming.sh
@@ -5,8 +5,10 @@ source scripts/env.sh
 docker run -d --restart unless-stopped  \
   --name $NAME_STREAMING \
   -h ispooge-streaming \
-  -v /tmp/hls:/tmp/hls \
-  -p 33935:1935 \
-  -p 33936:1936 \
-  --link $NAME_STATIC \
+  -v $PWD/nginx-conf:/etc/nginx:ro \
+  -v $ISPOOGE_MEDIA_PATH/hls:/tmp/http/hls:rw \
+  -v $ISPOOGE_MEDIA_PATH:/tmp/http/media:ro \
+  -v $PWD/app/resources/public:/tmp/http/cryogen:ro \
+  -p $RTMP_PORT:1935 \
+  -p $HTTP_PORT:1936 \
   $IMAGE_STREAMING
